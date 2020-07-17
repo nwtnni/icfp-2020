a263b3df48290f1124f9553bd75903b2768d61b2
Newton Ni <nwtnni@gmail.com>
2020-07-17 13:47:11 -0400

    Attempt to submit '1' to server

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/solution/target/release/submit "$@" || echo "run error code: $?"
+RUST_LOG="info" /solution/target/release/submit "$@" || echo "run error code: $?"
--- submit/main.rs
+++ submit/main.rs
@@ -1,7 +1,9 @@
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let _ = icfp::Client::new()?;
+    let client = icfp::Client::new()?;
+
+    client.send_alien_message(String::from("1"))?;
 
     Ok(())
 }
