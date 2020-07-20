62df30e38d5796a0501e1d46f8d2f76b906eaade
Newton Ni <nwtnni@gmail.com>
2020-07-20 03:17:58 -0400

    Re-enable logging to debug parser crash

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/solution/target/release/submit "$@" || echo "run error code: $?"
+RUST_BACKTRACE="full" RUST_LOG="icfp=debug,submit=info" /solution/target/release/submit "$@" || echo "run error code: $?"
