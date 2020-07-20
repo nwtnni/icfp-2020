f04312f5c9da9163b7f6c6f5e7c04e0ca4399957
Newton Ni <nwtnni@gmail.com>
2020-07-20 01:48:12 -0400

    Implement an attacking strategy
    - Attack closest enemy ship while maintaining temperature
    - Avoid accelerating at high temperature

--- submit/main.rs
+++ submit/main.rs
@@ -30,14 +30,13 @@ fn main() -> anyhow::Result<()> {
 
     log::info!("Initial State: {:#?}", initial);
 
-    let stats = game::Stats {
-        fuel: 252,
-        damage: 0,
-        coolant: 16,
-        bombs: 1,
+    let stats = match initial.info.role {
+    | game::Role::Attack => game::Stats { fuel: 124, damage: 64, coolant: 8, bombs: 1, },
+    | game::Role::Defend => game::Stats { fuel: 252, damage: 0, coolant: 16, bombs: 1, },
     };
 
     let mut current = client.start(&mut atoms, &stats)?;
+    let mut commands = Vec::new();
 
     let team = current.info.role;
 
@@ -49,25 +48,62 @@ fn main() -> anyhow::Result<()> {
 
         log::info!("Tick {}", state.tick);
 
-        let commands = state
-            .ships
-            .iter()
-            .filter(|(ship, _)| ship.role == team)
-            .map(|(ship, _)| {
-                let (dx, dy) = match (ship.x >= 0, ship.y >= 0) {
-                | (true, true) => (-1, 1),
-                | (false, true) => (-1, -1),
-                | (false, false) => (1, -1),
-                | (true, false) => (1, 1),
-                };
+        commands.clear();
 
-                game::Command::Accelerate {
-                    id: ship.id,
-                    x: dx,
-                    y: dy,
+        commands.extend(
+            state
+                .ships
+                .iter()
+                .filter(|(ship, _)| ship.role == team)
+                .flat_map(|(ship, _)| {
+                    if ship.temp > ship.max_temp / 2 {
+                        return None;
+                    }
+
+                    let (dx, dy) = match (ship.x >= 0, ship.y >= 0) {
+                    | (true, true) => (-1, 1),
+                    | (false, true) => (-1, -1),
+                    | (false, false) => (1, -1),
+                    | (true, false) => (1, 1),
+                    };
+
+                    Some(game::Command::Accelerate {
+                        id: ship.id,
+                        x: dx,
+                        y: dy,
+                    })
+                })
+        );
+
+        if team == game::Role::Attack {
+            for (ally, _) in state.ships.iter().filter(|(ship, _)| ship.role == team) {
+
+                if ally.temp > (ally.max_temp / 2 - ally.stats.damage) {
+                    continue;
                 }
-            })
-            .collect::<Vec<_>>();
+
+                let mut min_dist = i64::MAX;
+                let mut min_ship = None;
+
+                for (enemy, _) in state.ships.iter().filter(|(ship, _)| ship.role != team) {
+                    let dist = ((ally.x + ally.vx) - (enemy.x + enemy.vx)).pow(2) +
+                        ((ally.y + ally.vy) - (enemy.y + enemy.vy)).pow(2);
+
+                    if dist < min_dist {
+                        min_dist = dist;
+                        min_ship = Some(*enemy);
+                    }
+                }
+
+                if let Some(enemy) = min_ship {
+                    commands.push(game::Command::Shoot {
+                        id: enemy.id,
+                        x: enemy.x + enemy.vx,
+                        y: enemy.y + enemy.vy,
+                    });
+                }
+            }
+        }
 
         current = client.commands(&mut atoms, &commands)?;
     }
